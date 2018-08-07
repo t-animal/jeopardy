0497080ea6672738ec01f2cc2e0a6f10ab20d219
Tilman Adler <tilman-git@t-animal.de>
2018-08-07 20:56:33 +0200

    implement image answers

--- risiko-anwendung/ui/answers.py
+++ risiko-anwendung/ui/answers.py
@@ -2,13 +2,18 @@ import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
+from ..model.game import SpecialField
+
 class AnswerFactory:
 
     def __init__(self, playerManager):
         self.playerManager = playerManager
 
-    def createAnswer(self, category, text):
-        return TextAnswer(self.playerManager, category, text)
+    def createAnswer(self, category, answer):
+        if SpecialField.isSpecialField(answer) and answer.specialty == SpecialField.IMAGE_ANSWER:
+            return ImageAnswer(self.playerManager, category, answer.scalar)
+
+        return TextAnswer(self.playerManager, category, answer)
 
 class Answer(Gtk.Box):
 
@@ -24,5 +29,16 @@ class TextAnswer(Answer):
 
         self.pack_start(Gtk.Label(category), False, True, 0)
         self.pack_start(Gtk.Label(text), True, True, 0)
+
+        self.show_all()
+
+class ImageAnswer(Answer):
+    def __init__(self, playerManager, category, imageUrl):
+        super().__init__(playerManager)
+
+        self.set_orientation(Gtk.Orientation.VERTICAL)
+
+        self.pack_start(Gtk.Label(category), False, True, 0)
+        self.pack_start(Gtk.Image.new_from_file(imageUrl), True, True, 0)
 
         self.show_all()
